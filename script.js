415e363ef518f0c43658c5a152ee30c37ef4f26b
Justinas Pekarskis <jpekarskis@yahoo.com>
2021-12-03 16:34:57 +0200

    Random img change

--- script.js
+++ script.js
@@ -4,14 +4,20 @@ const wrapper = document.querySelector('.wrapper');
 const left = document.querySelector('.left');
 const right = document.querySelector('.right');
 
+
 let index = 0;
 let data =
     [
         "https://global.unitednations.entermediadb.net/assets/mediadb/services/module/asset/downloads/preset/Libraries/Production+Library/26-04-2021_Unsplash_forest.jpg/image1440x560cropped.jpg",
         "https://images.theconversation.com/files/305837/original/file-20191209-90562-nsnsun.jpg?ixlib=rb-1.1.0&rect=284%2C696%2C1934%2C965&q=45&auto=format&w=1356&h=668&fit=crop",
         "https://www.featureforest.ee/wp-content/uploads/2016/10/bg6.jpg",
+        "https://en.korpiforrest.fi/wp-content/uploads/2019/10/polku_004.jpg"
     ]
-right.onclick = () =>{
+right.onclick =  goRight;
+
+left.onclick = goLeft;
+
+function goRight(){
     index++;
     if(index > data.length - 1){
         index = 0;
@@ -19,15 +25,14 @@ right.onclick = () =>{
     const newImg = document.createElement('img');
     newImg.src = `${data[index]}`
     wrapper.append(newImg);
-    wrapper.scrollTo(1000, 0)
+    wrapper.scrollTo(wrapper.scrollWidth, 0)
     setTimeout( () => {
         wrapper.children[0].remove();
         newImg.classList.add('currentImg');
     }, 500)
-
 }
 
-left.onclick = () => {
+function goLeft(){
     index--;
     if(index < 0){
         index = data.length - 1;
@@ -47,6 +52,19 @@ left.onclick = () => {
         wrapper.children[wrapper.children.length - 1].remove();
     }, 500)
 }
+
+function randomDirection(){
+    if(Math.random() > 0.5){
+        goRight()
+    }else {
+        goLeft()
+    }
+}
+
+setInterval(randomDirection, 5000);
+
+
+
 
 
 
